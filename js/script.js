062c169f4f3f9897d4e3ab8c99409183eb37b53d
Gadzhidadaev Gadzhi <gadzhi7.7.7@mail.ru>
2018-02-05 15:16:56 +0300

    complete the desktop version of site

--- js/script.js
+++ js/script.js
@@ -1,16 +1,6 @@
 $(document).ready(function() {
 
-	var w = $('.work').css('width');
-	console.log(w);
-	var h = parseInt(w) / 100 *56.25;
-	$('.work').css('height', h + 'px');
-	$(window).resize(function(){	
-		var w = $('.work').css('width');
-		var h = parseInt(w) / 100 *56.25;
-		$('.work').css('height', h + 'px');
-	});
-
-
+/*
 
 	$('.menu__link').click(function(event) {
 		event.preventDefault();
@@ -65,15 +55,122 @@ $(document).ready(function() {
 		offset: 100    
 	});   
 	
+
 	
-	
-	
-	$('.portfolio__show-more').click(function() {
-		$('.work_part2').css({display: 'block'});
-		$(this).animate({opacity: 0}, 50);
-		$('.work_part2').animate({opacity: 1}, 500);
+	$('.works__slideshow').slick({
+		arrows: false,
+		speed: 8000,
+		centerMode: true,
+		variableWidth: true,
+		autoplay: true,
+		autoplaySpeed: 0,
+		cssEase: 'linear',
+		pauseOnHover: true
 	});
 	
+	
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+		
+	
+	
+	
+	
+	*/
+	
+	
+	
+	$('.menu__link').click(function(event) {
+		event.preventDefault();
+		var link =  $(this).attr('href');
+		$('html, body').animate({ scrollTop: $(link).offset().top - 70 }, 800);
+	});
+	
+	
+	
+	
+	
+	
+	
+	var w = $('.portfolio__photo img').css('width');
+	console.log(w);
+	var h = parseInt(w) / 100 *67;
+	$('.portfolio__photo img').css('height', h + 'px');
+	$(window).resize(function(){	
+		var w = $('.portfolio__photo img').css('width');
+		var h = parseInt(w) / 100 *67;
+		$('.portfolio__photo img').css('height', h + 'px');
+	});
 
+
+
+
+
+	
+	
+	
+	
+	
+/**************** header slideshow *****************************/
+
+	function Slider(obj) {
+		this.images = document.querySelectorAll(obj.images);
+		this.auto = obj.auto;	
+		this.btnPrev = obj.btnPrev;
+		this.btnNext = obj.btnNext;
+		this.rate = obj.rate || 1000;
+		var i = 0;
+		var slider = this;
+		this.prev = function () {
+			slider.images[i].classList.remove('showed');
+			i--;
+			
+			if (i < 0) {
+				i = slider.images.length - 1;
+			}
+
+			slider.images[i].classList.add('showed');
+		}
+		this.next = function () {
+			slider.images[i].classList.remove('showed');
+			i++;
+
+			if (i >= slider.images.length) {
+				i = 0;
+			}
+
+			slider.images[i].classList.add('showed');
+		}
+		document.querySelector(slider.btnPrev).onclick = slider.prev;
+		document.querySelector(slider.btnNext).onclick = slider.next;	
+		if(slider.auto)	{
+			setInterval(slider.next, slider.rate);
+		}
+	}
+	
+		
+	new Slider({
+		images: '.header__slide',
+		btnPrev: '.slide_prew',
+		btnNext: '.slide_next',
+		auto: true,
+		rate: 7000
+	});
+
+	
+	
 	
 });
