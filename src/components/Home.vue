77bac2d9c9d84442f52f011f214bcb905ebeacc4
gadzhi <gadzhidadaev95@gmail.com>
2019-03-13 10:19:56 +0300

    .

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -126,6 +126,28 @@
 </template>
 
 <script>
+let token = '5501043384.1677ed0.6f7fd36a2ebc4e2f840a939d729421a8',
+    userid = 228142647,
+    kolichestvo = 9;
+
+$.ajax({
+	url: 'https://api.instagram.com/v1/users/' + userid + '/media/recent/?' + token,
+	dataType: 'jsonp',
+	type: 'GET',
+	data: {access_token: token, count: kolichestvo}, // передаем параметры, которые мы указывали выше
+	success: function(result){
+ 		console.log(result);
+		for( x in result.data ){
+			$('ul').append('<li><img src="'+result.data[x].images.low_resolution.url+'"></li>'); // result.data[x].images.low_resolution.url - это URL картинки среднего разрешения, 306х306
+			// result.data[x].images.thumbnail.url - URL картинки 150х150
+			// result.data[x].images.standard_resolution.url - URL картинки 612х612
+			// result.data[x].link - URL страницы данного поста в Инстаграм
+		}
+	},
+	error: function(result){
+		console.log(result); // пишем в консоль об ошибках
+	}
+});
 export default {
   mounted() {
     // new Slider({
